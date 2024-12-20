d90e086bf796e1324fe711b68889450e6bd94bb0
Epp-code <7049620+Epp-code@users.noreply.github.com>
2024-12-19 23:17:04 -0800

    v0.5.5 - Rebuilt for SML v3.9 and latest build of Satisfactory

--- Source/AutoLink/Public/AutoLink.h
+++ Source/AutoLink/Public/AutoLink.h
@@ -14,7 +14,7 @@ DECLARE_LOG_CATEGORY_EXTERN(LogAutoLink, Verbose, All);
 
 // When we're building to ship, set this to 0 to no-op logging and debugging functions to minimize performance impact. Would prefer to do
 // this through build defines based on whether we're building for development or shipping but at the moment alpakit always builds shipping.
-#define AL_DEBUGGING 1
+#define AL_DEBUGGING 0
 
 class FAutoLinkModule : public FDefaultGameModuleImpl
 {
