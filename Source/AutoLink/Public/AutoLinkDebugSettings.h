fefc8f920f9455cc40c3c539a8119248df1276ee
Epp-code <7049620+Epp-code@users.noreply.github.com>
2025-06-10 12:19:41 -0700

    v0.10.0 - Updated for Satisfactory 1.1, including support for autolinking buffer stops and adding switch controls at the new junctions.

--- Source/AutoLink/Public/AutoLinkDebugSettings.h
+++ Source/AutoLink/Public/AutoLinkDebugSettings.h
@@ -2,7 +2,7 @@
 
 // When we're building to ship, set this to 0 to no-op logging and debugging functions to minimize performance impact. Would prefer to do
 // this through build defines based on whether we're building for development or shipping but at the moment alpakit always builds shipping.
-#define AL_DEBUG_ENABLED 1
+#define AL_DEBUG_ENABLED 0
 
 // Whether to enable mod functionality. Useful to disable mod while inspecting defualt functionality.
 #define AL_DEBUG_ENABLE_MOD (!AL_DEBUG_ENABLED || 1)
